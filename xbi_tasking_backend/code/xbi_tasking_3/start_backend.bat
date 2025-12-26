3d624f30c9c072f8f27b613bcb4654411ec8ac1c
redacted-link <limkonchern@gmail.com>
2025-12-26 19:13:53 +0800

    Fixed PS upload and sorted sample data

--- xbi_tasking_backend/code/xbi_tasking_3/start_backend.bat
+++ xbi_tasking_backend/code/xbi_tasking_3/start_backend.bat
@@ -8,7 +8,7 @@ REM Set Keycloak environment variables
 set KEYCLOAK_URL=http://localhost:8080
 set KEYCLOAK_REALM=xbi-tasking
 set KEYCLOAK_CLIENT_ID=xbi-tasking-backend
-set KEYCLOAK_CLIENT_SECRET=35Szo7P3WrSAVUEtImtBZDsuu0D41hYZ
+set KEYCLOAK_CLIENT_SECRET=CpYBsLztlLtlTPtHfbzB4w1k3lhSAcgK
 
 .\venv\Scripts\python.exe main.py dev_server.config
 pause
